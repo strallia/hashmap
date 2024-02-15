8eb6cccae05eb241196dda05f577b6d27e067cc1
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-02-15 13:23:55 -0800

    Write hash method to retrieve hashCode

--- src/HashMap.js
+++ src/HashMap.js
@@ -1,3 +1,15 @@
-export class HashMap() {
-  
+export class HashMap {
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = Math.floor((primeNumber * hashCode + key.charCodeAt(i)) % 16);
+    }
+
+    return hashCode;
+  }
 }
+
+let myHash = new HashMap();
+console.log(myHash.hash('test'));
