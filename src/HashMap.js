f13c99a5bedbe03562ebe289e286a6b95b12f3b6
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-02-15 13:41:05 -0800

    Write pseudocode for set method

--- src/HashMap.js
+++ src/HashMap.js
@@ -1,4 +1,6 @@
 export class HashMap {
+  hash = null;
+
   hash(key) {
     let hashCode = 0;
 
@@ -8,6 +10,16 @@ export class HashMap {
     }
 
     return hashCode;
+  }
+
+  set(key, value) {
+    // find hashCode for given key.
+    // if bucket === null, generate head node for starting new linked list
+    // with key and value properties passed and next set to null.
+    // else traverse list while checking if the curNode's key === key. If so,
+    // stop traversing and update the node's value with passed value.
+    // else if no key match was found, traverse to end of list to append
+    // new node
   }
 }
 
