fd4cf01bdbade6627088615b7555a0948b0dbdc6
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-02-15 13:56:36 -0800

    Write constructor for HashMap class

--- src/HashMap.js
+++ src/HashMap.js
@@ -1,6 +1,12 @@
 export class HashMap {
-  hash = null;
+  buckets = [];
   bucketsLength = 16;
+
+  constructor() {
+    for (let i = 0; i < this.bucketsLength; i++) {
+      this.buckets.push(null);
+    }
+  }
 
   hash(key) {
     let hashCode = 0;
@@ -10,7 +16,7 @@ export class HashMap {
       hashCode = Math.floor((primeNumber * hashCode + key.charCodeAt(i)) % 16);
     }
 
-    if (hashCode < 0 || hashCode >= bucketsLength) {
+    if (hashCode < 0 || hashCode >= this.bucketsLength) {
       throw new Error('Trying to access index out of bound');
     }
 
@@ -19,7 +25,7 @@ export class HashMap {
 
   set(key, value) {
     // find hashCode for given key.
-    // if bucket === null, generate head node for starting new linked list
+    // if bucket === null (ie contains no nodes), generate head node for starting new linked list
     // with key and value properties passed and next set to null.
     // else traverse list while checking if the curNode's key === key. If so,
     // stop traversing and update the node's value with passed value.
@@ -29,4 +35,4 @@ export class HashMap {
 }
 
 let myHash = new HashMap();
-console.log(myHash.hash('test'));
+console.log(myHash.buckets);
